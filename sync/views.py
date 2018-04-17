3bb1c3332fb95fd139926c9b6f36be049ebd3e09
Thor Kell <thor@tide-pool.ca>
2018-04-16 22:40:47 -0400

    Update mappings, lint and add docstrings to views.py

--- sync/views.py
+++ sync/views.py
@@ -14,30 +14,36 @@ def sync(request):
     authorized = auth(request)
     if request.method == 'POST' and authorized:
         actblue_data = json.loads(request.body)
-        ## This currently returns a list of dictionaries - unsure if we need to send one request per dictionary
+        # This currently returns a list of dictionaries
+        # unsure if we need to send one request per dictionary
         knack_values = transform(actblue_data)
         print("would have sent {} to knack".format(json.dumps(knack_values)))
         return HttpResponse('')
     else:
         return HttpResponseForbidden()
 
-
 def get_lineitems(actblue_values):
+    """
+    Parse information we need from ActBlue line items.
+
+    Returns a list of dictionaries, one dict per line item.
+    """
     knack_lineitems = []
     lineitems = actblue_values['lineitems']
 
     knack_lineitem = {}
     for lineitem in lineitems:
         knack_lineitem['889'] = lineitem.get('amount')
-        knack_lineitem['931'] = lineitem.get('entityId')
-        knack_lineitem['934'] = lineitem.get('committeeName')
-        knack_lineitem['940'] = lineitem.get('paidAt')
+        knack_lineitem['935'] = lineitem.get('entityId')
         knack_lineitems.append(knack_lineitem)
 
     return knack_lineitems
 
-
 def transform(actblue_values):
+    """
+    Transform ActBlue's data into a list of Knack-keyed dictionaries,
+    one per line item.
+    """
     knack_values = {}
     try:
         # this works in Python 2
@@ -45,9 +51,9 @@ def transform(actblue_values):
     except AttributeError:
         # this works in Python 3, and returns an generator like iteritems in Python 2
         mapping = settings.ACTBLUE_TO_KNACK_MAPPING.items()
-    for k, v in mapping:
-        path = k.split('#')
-        knack_values[v] = walk(path, actblue_values)
+    for key, value in mapping:
+        path = key.split('#')
+        knack_values[value] = walk(path, actblue_values)
 
     knack_lineitems = get_lineitems(actblue_values)
     for knack_lineitem in knack_lineitems:
@@ -56,6 +62,12 @@ def transform(actblue_values):
     return knack_lineitems
 
 def walk(path, container):
+    """
+    Recurse over the ActBlue JSON object, and get the values that we need,
+    based on the settings file.
+
+    Returns a single value for each path.
+    """
     if not container or isinstance(container, str):
         return None
     key = path[0]
@@ -70,7 +82,6 @@ def walk(path, container):
         else:
             new_container = container.get(path[0])
         return walk(path[1:], new_container)
-
 
 def auth(request):
     auth_header = request.META['HTTP_AUTHORIZATION']
--- sync_data/settings.py
+++ sync_data/settings.py
@@ -6,6 +6,7 @@ ACTBLUE_TO_KNACK_MAPPING = {
     'contribution#createdAt': '888',
     'contribution#recurringDuration': '890',
     'contribution#contributionForm': '893',
+    'contribution#refcode': '896',
     'donor#firstname': '897',
     'donor#lastname': '898',
     'donor#addr1': '899',
@@ -15,6 +16,6 @@ ACTBLUE_TO_KNACK_MAPPING = {
     'donor#country': '904',
     'donor#employerData#occupation': '905',
     'donor#employerData#employer': '906',
-    'donor#email': '907',
+    'donor#email': '908',
     'donor#phone': '909', # String, not an int
 }
