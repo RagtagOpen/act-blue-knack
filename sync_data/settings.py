846aa5dd39e166393b8237644b1ba16d561fd2c6
Thor Kell <thor@tide-pool.ca>
2018-04-17 21:31:02 -0400

    Correct data in settings and test_settings

--- sync_data/settings.py
+++ sync_data/settings.py
@@ -3,19 +3,19 @@ from .base_settings import *
 DEBUG = False
 
 ACTBLUE_TO_KNACK_MAPPING = {
-    'contribution#createdAt': '888',
-    'contribution#recurringDuration': '890',
-    'contribution#contributionForm': '893',
-    'contribution#refcode': '896',
-    'donor#firstname': '897',
-    'donor#lastname': '898',
-    'donor#addr1': '899',
-    'donor#city': '901',
-    'donor#state': '902',
-    'donor#zip': '903', # String, not an int
-    'donor#country': '904',
-    'donor#employerData#occupation': '905',
-    'donor#employerData#employer': '906',
-    'donor#email': '908',
-    'donor#phone': '909', # String, not an int
+    'contribution#createdAt': '638',
+    'contribution#recurringDuration': '640',
+    'contribution#contributionForm': '643',
+    'contribution#refcode': '646',
+    'donor#firstname': '647',
+    'donor#lastname': '648',
+    'donor#addr1': '649',
+    'donor#city': '651',
+    'donor#state': '652',
+    'donor#zip': '653', # String, not an int
+    'donor#country': '654',
+    'donor#employerData#occupation': '655',
+    'donor#employerData#employer': '656',
+    'donor#email': '874',
+    'donor#phone': '658', # String, not an int
 }
