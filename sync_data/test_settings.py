b0d8219e700b55447addf2b6bac973f048654df8
Thor Kell <thor@tide-pool.ca>
2018-04-30 19:51:35 -0400

    Add KNACK_OBJECT_ID to test_settings

--- sync_data/test_settings.py
+++ sync_data/test_settings.py
@@ -1,5 +1,7 @@
 from .actblue_mappings.testing import (ACTBLUE_TO_KNACK_MAPPING_ARRAY_ITEMS,
-                                       ACTBLUE_TO_KNACK_MAPPING_SCALARS)
+                                       ACTBLUE_TO_KNACK_MAPPING_SCALARS,
+                                       KNACK_OBJECT_ID
+                                       )
 from .base_settings import *
 
 DEBUG = True
