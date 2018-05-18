08050f18aff5d1bd9c44c0235e1caebbc6859301
Jill Huchital <jill.huchital@gmail.com>
2018-05-18 08:22:13 -0700

    fixed object and fields per Sister District

--- sync_data/actblue_mappings/testing.py
+++ sync_data/actblue_mappings/testing.py
@@ -1,6 +1,5 @@
 ACTBLUE_TO_KNACK_MAPPING_SCALARS = {
     'contribution#createdAt': 'field_888',
-    'contribution#recurringDuration': 'field_890',
     'contribution#contributionForm': ['field_893', 'field_922'],
     'contribution#refcode': 'field_896',
     'donor#firstname': 'field_897',
@@ -10,8 +9,6 @@ ACTBLUE_TO_KNACK_MAPPING_SCALARS = {
     'donor#state': 'field_902',
     'donor#zip': 'field_903',  # String, not an int
     'donor#country': 'field_904',
-    'donor#employerData#occupation': 'field_905',
-    'donor#employerData#employer': 'field_906',
     'donor#email': 'field_908',
     'donor#phone': 'field_909',  # String, not an int
 }
