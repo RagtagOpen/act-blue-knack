2d247e04d273f9a67f3b53bd8e329ad17e3ba1f1
dryan <dryan@users.noreply.github.com>
2019-06-18 17:00:21 -0400

    setting a DATABASES setting so manage.py test can run

--- sync_data/test_settings.py
+++ sync_data/test_settings.py
@@ -1,10 +1,16 @@
-from .actblue_mappings.testing import (ACTBLUE_TO_KNACK_MAPPING_ARRAY_ITEMS,
-                                       ACTBLUE_TO_KNACK_MAPPING_SCALARS,
-                                       KNACK_DONOR_REQUIRED_FIELDS,
-                                       KNACK_OBJECT_ID,
-                                       FIELD_PREFIXES,
-                                       TIMEZONE_CONVERSION_NEEDED
-                                       )
+from .actblue_mappings.testing import (
+    ACTBLUE_TO_KNACK_MAPPING_ARRAY_ITEMS,
+    ACTBLUE_TO_KNACK_MAPPING_SCALARS,
+    FIELD_PREFIXES,
+    KNACK_DONOR_REQUIRED_FIELDS,
+    KNACK_OBJECT_ID,
+    TIMEZONE_CONVERSION_NEEDED,
+)
 from .base_settings import *
 
 DEBUG = True
+
+# this is needed for running tests
+DATABASES = {
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "actblue-knack"}
+}
